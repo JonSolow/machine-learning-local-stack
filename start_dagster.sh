009428abd31bcdcb341773ddf88e9486a8038236
Jon Solow <jssolow+gh@gmail.com>
2024-04-22 01:55:27 +0000

    Add script to start dagster

---
+++ start_dagster.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+REPO_ROOT_DIR=`git rev-parse --show-toplevel`
+cd $REPO_ROOT_DIR/dagster-dbt/dagster_project
+
+DAGSTER_DBT_PARSE_PROJECT_ON_LOAD=1 \
+DAGSTER_HOME=$REPO_ROOT_DIR/dagster-dbt/data \
+pipenv run dagster dev
