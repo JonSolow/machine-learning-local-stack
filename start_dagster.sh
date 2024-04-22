d746b31fba5005f7e20b40469392c289a27b9e78
Jon Solow <jssolow+gh@gmail.com>
2024-04-22 02:02:00 +0000

    Fix data dir ref in start_dagster.sh

--- start_dagster.sh
+++ start_dagster.sh
@@ -4,5 +4,5 @@ REPO_ROOT_DIR=`git rev-parse --show-toplevel`
 cd $REPO_ROOT_DIR/dagster-dbt/dagster_project
 
 DAGSTER_DBT_PARSE_PROJECT_ON_LOAD=1 \
-DAGSTER_HOME=$REPO_ROOT_DIR/dagster-dbt/data \
+DAGSTER_HOME=$REPO_ROOT_DIR/data \
 pipenv run dagster dev
