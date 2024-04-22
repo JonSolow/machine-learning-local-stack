7a2982d407770895712ce70da78b1827929b2e82
Jon Solow <jssolow+gh@gmail.com>
2024-04-22 03:17:07 +0000

    Add Dockerfile for metabase to build image compatible with duckdb driver per https://github.com/AlexR2D2/metabase_duckdb_driver/tree/0.2.6?tab=readme-ov-file#docker

--- start_metabase.sh
+++ start_metabase.sh
@@ -2,12 +2,15 @@
 
 
 REPO_ROOT_DIR=`git rev-parse --show-toplevel`
-METABASE_LOCAL_DATA_DIR="${REPO_ROOT_DIR}/data/metabase-data"
+METABASE_LOCAL_DATA_DIR="${REPO_ROOT_DIR}/data/metabase-data/"
 
 METABASE_PORT=6382
 
-docker run --rm -d \
+docker build . -f metabase/Dockerfile --tag metaduck:latest
+
+docker run --rm -it -d \
   -p $METABASE_PORT:3000 \
   -v $METABASE_LOCAL_DATA_DIR:/metabase-data \
   -e "MB_DB_FILE=/metabase-data/metabase.db" \
-  --name metabase metabase/metabase
+  -e "MB_PLUGINS_DIR=/home/plugins" \
+  --name metabase metaduck
