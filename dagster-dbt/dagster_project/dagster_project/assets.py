212cf60ea2433d38df03e1401dbfd9a2adfeba19
Jon Solow <jssolow+gh@gmail.com>
2024-07-04 04:43:53 +0000

    Add dlt source as dagster asset using dagster_embedded_elt

--- dagster-dbt/dagster_project/dagster_project/assets.py
+++ dagster-dbt/dagster_project/dagster_project/assets.py
@@ -1,5 +1,8 @@
 from dagster import AssetExecutionContext
 from dagster_dbt import DbtCliResource, dbt_assets
+from dagster_embedded_elt.dlt import DagsterDltResource, dlt_assets
+from dlt import pipeline
+from dlt_sources.pokemon import source as pokemon_source
 
 from .constants import dbt_manifest_path
 
@@ -7,4 +10,18 @@ from .constants import dbt_manifest_path
 @dbt_assets(manifest=dbt_manifest_path)
 def dbt_project_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
-    
+
+
+@dlt_assets(
+    dlt_source=pokemon_source(),
+    dlt_pipeline=pipeline(
+        pipeline_name="pokemon",
+        dataset_name="pokemon_data",
+        destination="duckdb",
+        progress="log",
+    ),
+    name="pokemon",
+    group_name="pokemon",
+)
+def dlt_pokemon_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
+    yield from dlt.run(context=context)
--- dagster-dbt/dagster_project/dagster_project/definitions.py
+++ dagster-dbt/dagster_project/dagster_project/definitions.py
@@ -2,15 +2,18 @@ import os
 
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
+from dagster_embedded_elt.dlt import DagsterDltResource
 
-from .assets import dbt_project_dbt_assets
+
+from .assets import dbt_project_dbt_assets, dlt_pokemon_assets
 from .constants import dbt_project_dir
 from .schedules import schedules
 
 defs = Definitions(
-    assets=[dbt_project_dbt_assets],
+    assets=[dbt_project_dbt_assets, dlt_pokemon_assets],
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
+        "dlt": DagsterDltResource(),
     },
 )
